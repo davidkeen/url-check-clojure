f5645ba6236a349fbc6a875751c4e645707db4b9
David Keen <dkkeen@gmail.com>
2016-08-06 00:53:53 +0100

    Use let instead of def

--- src/url_check/core.clj
+++ src/url_check/core.clj
@@ -9,14 +9,28 @@
            "http://nope"])
 
 (defn check-url
-  [s]
+  "Checks a URL. Anything other than a 200-207/300-307 response is considered a failure."
+  [url]
   (try
-    (client/get s)
-    (println (str "[OK] " s))
+    (client/get url)
+    {:url url :status "OK"}
     (catch Exception e
-      (println (str "[FAIL] " s " - " (.getMessage e)))))
-  )
+      {:url url :status "FAIL" :message (.getMessage e)})))
+
+(defn filter-status
+  "Filters results with the given status. Expects a list of maps."
+  [coll status]
+  (filter (fn [s] (= (:status s) status)) coll))
 
 (defn -main
   [& args]
-  (doseq [s urls] (check-url s)))
+  (println "Checking" (count urls) "URLs")
+  (let [results (map check-url urls)]
+    (println "OK:" (count (filter-status results "OK")))
+    (println "Failed:" (count (filter-status results "FAIL")))
+    (doseq [x (filter-status results "FAIL")] (println \tab (:url x) (:message x)))))
+
+;; Things to do:
+;; Fetch URLs from database
+;; Check URLs in parallel
+;; Send email with failures
