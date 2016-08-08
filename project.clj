53b36d772e10b95537afa80299885c51cb482b1b
David Keen <dkkeen@gmail.com>
2016-08-08 21:07:58 +0100

    Update licence in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject url-check "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.2.0"]
                  [org.clojure/java.jdbc "0.6.2-alpha2"]
